ea0ad99bc0a79ac7e6dd34540e0ca694dff6bee8
clancyfogarty <clancyfogarty@gmail.com>
2017-08-02 19:55:46 -0700

    revised issues

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,11 @@
 var animatePoints = function() {
 
-var revealPoint = function (index) {
    var revealPoint = function() {
      $(this).css({
      opacity: 1,
      transform: 'scaleX(1) translateY(0)'
  });
+
 };
   $.each($('.point'), revealPoint);
 };
@@ -14,6 +14,7 @@ var revealPoint = function (index) {
       if ($(window).height() > 950) {
           animatePoints();
       }
+      
       var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
       $(window).scroll(function(event) {
